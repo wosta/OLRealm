5f43ebb9674733ec7d0c8a11208880e12c4fd5d9
wosta <weiwang1202@126.com>
2016-07-19 17:10:11 +0800

    add base code

---
+++ OLRealm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OLRealm
+//
+//  Created by olive on 16/7/19.
+//  Copyright © 2016年 olive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OLRealm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OLRealm
+//
+//  Created by olive on 16/7/19.
+//  Copyright © 2016年 olive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
